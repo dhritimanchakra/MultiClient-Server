72aa0f21a568e6ece874393c5bec5398a40a8091
Dhrimanti <dhritimancg@gmail.com>
2026-02-17 19:53:15 +0530

    Started making again

--- pollserver.c
+++ pollserver.c
@@ -103,5 +103,18 @@ void del_from_pfds(struct pollfd pfds[],int i,int *fd_count){
 
 void handle_new_connection(int listener,int *fd_count,int *fd_size,struct pollfd **pfds){
     struct sockaddr_storage remoteaddr;
+    socklen_t addrlen;
+    int newfd;
+    char remoteIP[INET6_ADDRSTRLEN];
+    addrlen=sizeof remoteaddr;
+    newfd=accept(listener,(struct sockaddr *)&remoteaddr,&addrlen);
+    if(newfd==-1){
+        perror("accept");
+    }else{
+        add_to_pfds(pfds,newfd,fd_count,fd_size);
+        printf("pollserver: new connection from %s on socket %d\n",
+                inet_ntop2(&remoteaddr, remoteIP, sizeof remoteIP),
+                newfd);
+    }
 
 }
